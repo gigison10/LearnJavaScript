283ff977475a868286b57602eb40bb4994774d26
Eugen <eugen.izman@gmail.com>
2021-05-24 23:21:14 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,56 @@
-"use strict"
+//Functions
+
+// 1 CallBack
+
+/*function calcSumm(numOne, numTwo, more, less,) {
+    let numSumm = numOne + numTwo;
+
+    if (numSumm > 10) {
+        more();
+    } else {
+        less();
+    }
+}
+
+function showMore() { 
+    console.log("More then 10");
+}
+function showLess() {
+    console.log("Less then 10");
+}
+calcSumm (11, 5, showMore, showLess);
 
 
-let firstName = "gigison";
+// 2 Recursive ?????????????????
+
+function summ(numFirst, numSecond) {
+    if (numSecond ===1) {
+        return numFirst;
+    } else {
+        return numFirst * summ(numFirst, numSecond -1);
+    }
+ }
+console.log(summ(5, 3));*/
+
+////////////////22222222222222/////////////////?????????
+//https://htmllab.ru/zadachi-po-javascript/
+
+/*function calcSumm(firstName, more, less,) {
+
+    if (firstName = String) {
+        more();
+    } else {
+        less();
+    }
+}
+function helloName() { 
+    console.log("Hello Vasile");
+}
+function helloGuest() {
+    console.log("Hello Guest");
+}
+calcSumm ("Vasile", helloName, helloGuest);*/
+
+////////////////222222222222//////////????????????????
+
+
